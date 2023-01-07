3851e6e17b85f68d1507b822b3605fb5bdbfb96f
profitsw2000 <profitsw2000@gmail.com>
2023-01-07 15:24:53 +0300

    Переход на экраны регистрации и восстаговления пароля.

--- app/src/main/java/ru/profitsw2000/diarynote/presentation/MainActivity.kt
+++ app/src/main/java/ru/profitsw2000/diarynote/presentation/MainActivity.kt
@@ -2,12 +2,15 @@ package ru.profitsw2000.diarynote.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import diarynote.core.common.Controller
 import diarynote.passwordrecovery.presentation.view.PasswordRecoveryFragment
 import diarynote.registrationscreen.presentation.view.RegistrationFragment
 import diarynote.signinscreen.presentation.view.SignInFragment
 import ru.profitsw2000.app.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Controller {
+
+    private val fragmentManager by lazy { supportFragmentManager }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +22,39 @@ class MainActivity : AppCompatActivity() {
             it.hide()
         }
 
-        supportFragmentManager.apply {
+        fragmentManager.apply {
             beginTransaction()
                 .replace(R.id.fragment_container, SignInFragment.newInstance())
+                .commitAllowingStateLoss()
+        }
+    }
+
+    override fun openRegistrationFragment() {
+        fragmentManager.apply {
+            beginTransaction()
+                .setCustomAnimations(
+                    diarynote.core.R.anim.slide_in,
+                    diarynote.core.R.anim.fade_out,
+                    diarynote.core.R.anim.fade_in,
+                    diarynote.core.R.anim.slide_out
+                )
+                .replace(R.id.fragment_container, RegistrationFragment.newInstance())
+                .addToBackStack("")
+                .commitAllowingStateLoss()
+        }
+    }
+
+    override fun openPasswordRecoveryFragment() {
+        fragmentManager.apply {
+            beginTransaction()
+                .setCustomAnimations(
+                    diarynote.core.R.anim.slide_in,
+                    diarynote.core.R.anim.fade_out,
+                    diarynote.core.R.anim.fade_in,
+                    diarynote.core.R.anim.slide_out
+                )
+                .replace(R.id.fragment_container, PasswordRecoveryFragment.newInstance())
+                .addToBackStack("")
                 .commitAllowingStateLoss()
         }
     }
