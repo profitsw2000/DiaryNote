1ba9c330e5656138c48133e07d3822cadb6ee10f
profitsw2000 <profitsw2000@gmail.com>
2023-01-07 13:29:49 +0300

    Сделан переход на экран регистрации.

--- signInScreen/src/main/java/diarynote/signinscreen/presentation/view/SignInFragment.kt
+++ signInScreen/src/main/java/diarynote/signinscreen/presentation/view/SignInFragment.kt
@@ -1,9 +1,11 @@
 package diarynote.signinscreen.presentation.view
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import diarynote.core.common.Controller
 import diarynote.core.view.CoreFragment
 import diarynote.signinscreen.R
 import diarynote.signinscreen.databinding.FragmentSignInBinding
@@ -12,6 +14,14 @@ class SignInFragment : CoreFragment(R.layout.fragment_sign_in) {
 
     private var _binding: FragmentSignInBinding? = null
     private val binding get() = _binding!!
+    private val controller by lazy { activity as Controller }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (activity !is Controller) {
+            throw IllegalStateException("Activity должна наследоваться от Controller")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +29,14 @@ class SignInFragment : CoreFragment(R.layout.fragment_sign_in) {
     ): View {
         _binding = FragmentSignInBinding.bind(inflater.inflate(R.layout.fragment_sign_in, container, false))
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.registrationButton.setOnClickListener {
+            controller.openRegistrationFragment()
+        }
     }
 
     override fun onDestroy() {
