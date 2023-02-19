f02bc3815c3fa498bf77629bca646f4cedf8d006
profitsw2000 <profitsw2000@gmail.com>
2023-02-19 14:53:36 +0300

    Экран восстановления пароля с фейковой отправкой пароля на электронную почту.

--- passwordRecovery/src/main/java/diarynote/passwordrecovery/presentation/viewmodel/PasswordRecoveryViewModel.kt
+++ passwordRecovery/src/main/java/diarynote/passwordrecovery/presentation/viewmodel/PasswordRecoveryViewModel.kt
@@ -1,7 +1,49 @@
 package diarynote.passwordrecovery.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import diarynote.core.utils.EMAIL_BIT_NUMBER
+import diarynote.core.utils.EMAIL_PATTERN
+import diarynote.core.utils.InputValidator
+import diarynote.core.utils.ROOM_BIT_NUMBER
 import diarynote.core.viewmodel.CoreViewModel
+import diarynote.data.interactor.UserInteractor
+import diarynote.passwordrecovery.model.RecoveryState
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 
-class PasswordRecoveryViewModel : CoreViewModel() {
-    // TODO: Implement the ViewModel
+class PasswordRecoveryViewModel(
+    private val userInteractor: UserInteractor
+) : CoreViewModel() {
+
+    private val inputValidator = InputValidator()
+
+    private val _recoveryLiveData = MutableLiveData<RecoveryState>()
+    val recoveryState: LiveData<RecoveryState> by this::_recoveryLiveData
+
+    fun passwordRecovery(email: String) {
+        val emailIsValid = inputValidator.checkInputIsValid(email, EMAIL_PATTERN)
+
+        if (emailIsValid) {
+            sendEmail(email)
+        } else {
+            _recoveryLiveData.value = RecoveryState.Error(1 shl EMAIL_BIT_NUMBER)
+        }
+    }
+
+    private fun sendEmail(email: String) {
+        _recoveryLiveData.value = RecoveryState.Loading
+
+        userInteractor.getUserByEmail(email, false)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    _recoveryLiveData.value = RecoveryState.Success
+                },
+                {
+                    _recoveryLiveData.value = RecoveryState.Error(1 shl ROOM_BIT_NUMBER)
+                }
+            )
+    }
 }
