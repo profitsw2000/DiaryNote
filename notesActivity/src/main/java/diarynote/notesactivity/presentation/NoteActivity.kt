cd612ae646164cec713c3cebb84e0ba5f4c614ae
profitsw2000 <profitsw2000@gmail.com>
2023-04-16 09:53:52 +0300

    Навигация по фрагментам (тестовые фрагменты) на экране с заметками.

--- notesActivity/src/main/java/diarynote/notesactivity/presentation/NoteActivity.kt
+++ notesActivity/src/main/java/diarynote/notesactivity/presentation/NoteActivity.kt
@@ -2,11 +2,35 @@ package diarynote.notesactivity.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import diarynote.notesactivity.R
+import diarynote.notesactivity.databinding.ActivityNoteBinding
 
 class NoteActivity : AppCompatActivity() {
+
+    private var _binding: ActivityNoteBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note)
+        _binding = ActivityNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.fragment_container
+        ) as NavHostFragment
+
+        navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.main, R.id.categories, R.id.calendar, R.id.settings)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
