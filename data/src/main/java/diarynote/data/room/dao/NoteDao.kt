9218b4dc7ff58df90e10df9e2a53cb290fd26243
profitsw2000 <profitsw2000@gmail.com>
2023-04-28 16:42:25 +0300

    Сделаны функции добавления, удаления и обновления заметки в таблице для NoteDao.kt.

--- data/src/main/java/diarynote/data/room/dao/NoteDao.kt
+++ data/src/main/java/diarynote/data/room/dao/NoteDao.kt
@@ -1,7 +1,20 @@
 package diarynote.data.room.dao
 
-import androidx.room.Dao
+import androidx.room.*
+import diarynote.data.room.entity.NoteEntity
+import diarynote.data.room.entity.UserEntity
+import io.reactivex.rxjava3.core.Completable
 
 @Dao
 interface NoteDao {
+
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    fun insert(noteEntity: NoteEntity): Completable
+
+    @Update
+    fun update(noteEntity: NoteEntity): Completable
+
+    @Delete
+    fun delete(noteEntity: NoteEntity): Completable
 }
