d7e88f2631ea2b23980599cdc75dd9f2e24fbb3a
profitsw2000 <profitsw2000@gmail.com>
2023-04-28 22:24:53 +0300

    Создан класс для считывания списка категорий, принадлежащих конкретному пользователю. В класс объекта доступа таблицы с категориями добавлен метод для чтения списка категорий, принадлежащих пользователю с определённым id.

--- data/src/main/java/diarynote/data/room/dao/CategoryDao.kt
+++ data/src/main/java/diarynote/data/room/dao/CategoryDao.kt
@@ -1,7 +1,23 @@
 package diarynote.data.room.dao
 
-import androidx.room.Dao
+import androidx.room.*
+import diarynote.data.room.entity.CategoryEntity
+import diarynote.data.room.related.UserWithCategories
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
 
 @Dao
 interface CategoryDao {
+    @Transaction
+    @Query("SELECT * FROM UserEntity WHERE id LIKE :id")
+    fun getUserWithCategories(id: Int): Single<UserWithCategories>
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    fun insert(categoryEntity: CategoryEntity): Completable
+
+    @Update
+    fun update(categoryEntity: CategoryEntity): Completable
+
+    @Delete
+    fun delete(categoryEntity: CategoryEntity): Completable
 }
