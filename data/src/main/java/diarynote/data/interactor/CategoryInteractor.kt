0596f24ab36c82333951ce058da51550ac69b814
profitsw2000 <profitsw2000@gmail.com>
2023-04-30 20:56:58 +0300

    Сделан интерактор CategoryInteractor.kt для получения данных по категориям.

--- data/src/main/java/diarynote/data/interactor/CategoryInteractor.kt
+++ data/src/main/java/diarynote/data/interactor/CategoryInteractor.kt
@@ -1,4 +1,23 @@
 package diarynote.data.interactor
 
-class CategoryInteractor {
+import diarynote.data.domain.local.CategoryRepositoryLocal
+import diarynote.data.domain.local.UserRepositoryLocal
+import diarynote.data.domain.web.CategoryRepositoryRemote
+import diarynote.data.room.entity.CategoryEntity
+import diarynote.data.room.related.UserWithCategories
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
+
+class CategoryInteractor(
+    private val categoryRepositoryLocal: CategoryRepositoryLocal,
+    private val categoryRepositoryRemote: CategoryRepositoryRemote
+) {
+
+    fun getAllUserCategories(userId: Int): Single<UserWithCategories> {
+        return categoryRepositoryLocal.getAllUserCategories(userId)
+    }
+
+    fun addCategory(categoryEntity: CategoryEntity): Completable {
+        return categoryRepositoryLocal.addCategory(categoryEntity)
+    }
 }
