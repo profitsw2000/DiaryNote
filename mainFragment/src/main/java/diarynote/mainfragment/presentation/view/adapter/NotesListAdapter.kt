6990823d893e6b54863b24dcbf82ac9d971f538c
profitsw2000 <profitsw2000@gmail.com>
2023-05-06 22:17:59 +0300

    Сделан адаптер для списка заметок.

---
+++ mainFragment/src/main/java/diarynote/mainfragment/presentation/view/adapter/NotesListAdapter.kt
@@ -0,0 +1,56 @@
+package diarynote.mainfragment.presentation.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import diarynote.data.model.NoteModel
+import diarynote.mainfragment.databinding.NotesListItemViewBinding
+import java.text.SimpleDateFormat
+
+class NotesListAdapter : RecyclerView.Adapter<NotesListAdapter.ViewHolder>() {
+
+    private lateinit var binding: NotesListItemViewBinding
+    private var data: List<NoteModel> = arrayListOf()
+
+    fun setData(data: List<NoteModel>) {
+        this.data = data
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesListAdapter.ViewHolder {
+        binding = NotesListItemViewBinding.inflate(LayoutInflater.from(parent.context),
+            parent,
+            false)
+        return ViewHolder(binding.root)
+    }
+
+    override fun onBindViewHolder(holder: NotesListAdapter.ViewHolder, position: Int) {
+        holder.bind(data[position], position)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(noteModel: NoteModel, position: Int) {
+            with(binding) {
+                noteTitleTextView.text = noteModel.title
+                noteContentTextView.text = noteModel.text
+                tagsTextView.text = fromListToTagsString(noteModel.tags)
+                creationDateTextView.text = SimpleDateFormat("dd.MM.yyyy").format(noteModel.date)
+                if (noteModel.edited) editedNoteSignImageView.setImageResource(diarynote.core.R.drawable.edit_icon_24)
+            }
+        }
+
+        private fun fromListToTagsString(stringList: List<String>) : String {
+            var tags = ""
+            stringList.forEach {
+                tags.plus("#$it ")
+            }
+            return tags
+        }
+    }
+}
