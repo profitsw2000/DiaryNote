7d7b97e273fc2668bb3a5f9497ac486487fa5642
profitsw2000 <profitsw2000@gmail.com>
2023-05-07 17:06:05 +0300

    Считывание списка заметок пользователя из базы данных при открытии вкладки "Главная". Пока список отображается только в лог.

--- mainFragment/src/main/java/diarynote/mainfragment/presentation/view/MainFragment.kt
+++ mainFragment/src/main/java/diarynote/mainfragment/presentation/view/MainFragment.kt
@@ -1,17 +1,23 @@
 package diarynote.mainfragment.presentation.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import diarynote.mainfragment.R
 import diarynote.mainfragment.databinding.FragmentMainBinding
+import diarynote.mainfragment.model.NotesState
+import diarynote.mainfragment.presentation.viewmodel.HomeViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+    private val homeViewModel: HomeViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +26,33 @@ class MainFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentMainBinding.bind(inflater.inflate(R.layout.fragment_main, container, false))
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    private fun observeData() {
+        val observer = Observer<NotesState> { renderData(it)}
+        homeViewModel.notesLiveData.observe(viewLifecycleOwner, observer)
+    }
+
+    private fun renderData(notesState: NotesState) {
+        when(notesState) {
+            is NotesState.Success -> Log.d("VVV", notesState.noteModelList.toString())
+            is NotesState.Loading -> showProgressBar()
+            is NotesState.Error -> handleError(notesState.message)
+            else -> {}
+        }
+    }
+
+    private fun handleError(message: String) {
+
+    }
+
+    private fun showProgressBar() {
+        
     }
 
     override fun onDestroyView() {
