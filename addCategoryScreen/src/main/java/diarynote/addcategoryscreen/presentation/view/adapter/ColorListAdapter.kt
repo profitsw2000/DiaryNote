43937157267aef87bdfedfee15fc4437430d535d
profitsw2000 <profitsw2000@gmail.com>
2023-05-31 11:23:50 +0300

    Сделан адаптер для списка выбора цвета категории.

--- addCategoryScreen/src/main/java/diarynote/addcategoryscreen/presentation/view/adapter/ColorListAdapter.kt
+++ addCategoryScreen/src/main/java/diarynote/addcategoryscreen/presentation/view/adapter/ColorListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import diarynote.addcategoryscreen.databinding.CategoryColorPickerRecyclerviewItemBinding
+import diarynote.addcategoryscreen.model.ColorModel
 import diarynote.core.utils.listener.OnItemClickListener
 
 class ColorListAdapter (
@@ -12,6 +13,20 @@ class ColorListAdapter (
 ) : RecyclerView.Adapter<ColorListAdapter.ViewHolder>() {
 
     private lateinit var binding: CategoryColorPickerRecyclerviewItemBinding
+    private var data: List<ColorModel> = arrayListOf()
+    private var lastClickedPosition = 0
+    private val addElement = ColorModel(0, false)
+
+    fun setData(data: List<ColorModel>) {
+        this.data = data + addElement
+        notifyDataSetChanged()
+    }
+
+    fun setData(data: List<ColorModel>, selectedItemPosition: Int) {
+        lastClickedPosition = selectedItemPosition
+        this.data = data + addElement
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         binding = CategoryColorPickerRecyclerviewItemBinding.inflate(
@@ -22,14 +37,21 @@ class ColorListAdapter (
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return data.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(data[position])
     }
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(colorModel: ColorModel) = with(binding) {
+            colorPickerRecyclerViewItemCardView.setCardBackgroundColor(colorModel.color)
+            if (colorModel.isSelected) colorPickerRecyclerViewItemCardView.strokeWidth = 2
 
+            colorPickerRecyclerViewItemCardView.setOnClickListener {
+                onItemClickListener.onItemClick(layoutPosition)
+            }
+        }
     }
 }
