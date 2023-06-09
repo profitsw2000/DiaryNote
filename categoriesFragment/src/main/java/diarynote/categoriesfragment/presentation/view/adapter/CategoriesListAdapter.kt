4436f03629addf93494825a36f2cb6cecd4569bf
profitsw2000 <profitsw2000@gmail.com>
2023-06-09 21:47:12 +0300

    Переделан адаптер главного списка категорий.

--- categoriesFragment/src/main/java/diarynote/categoriesfragment/presentation/view/adapter/CategoriesListAdapter.kt
+++ categoriesFragment/src/main/java/diarynote/categoriesfragment/presentation/view/adapter/CategoriesListAdapter.kt
@@ -5,6 +5,7 @@ import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import diarynote.categoriesfragment.databinding.CategoriesListItemBinding
@@ -13,7 +14,6 @@ import diarynote.data.model.CategoryModel
 class CategoriesListAdapter : RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
 
     private var data: List<CategoryModel> = arrayListOf()
-    private lateinit var binding: CategoriesListItemBinding
 
     fun setData(data: List<CategoryModel>) {
         this.data = data
@@ -21,15 +21,17 @@ class CategoriesListAdapter : RecyclerView.Adapter<CategoriesListAdapter.ViewHol
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        binding = CategoriesListItemBinding.inflate(LayoutInflater.from(parent.context),
+        val binding = CategoriesListItemBinding.inflate(LayoutInflater.from(parent.context),
                                                     parent,
                                                     false)
-        return ViewHolder(binding.root)
-    }
 
-    override fun getItemViewType(position: Int): Int {
-        return super.getItemViewType(position)
-        return position
+        val categoryViewHolder = ViewHolder(binding)
+
+        binding.root.setOnClickListener {
+            Toast.makeText(parent.context, categoryViewHolder.adapterPosition.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+        return categoryViewHolder
     }
 
     override fun getItemCount(): Int {
@@ -37,48 +39,51 @@ class CategoriesListAdapter : RecyclerView.Adapter<CategoriesListAdapter.ViewHol
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(data[position], position)
+        val categoryModel = data[position]
+
+        with(holder){
+            cardView.setCardBackgroundColor(categoryModel.color)
+            cardView.layoutParams = getViewParams(position)
+            imageView.setImageResource(getImageFromResources(categoryModel.categoryImage))
+            title.text = categoryModel.categoryName
+        }
     }
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        fun bind(categoryModel: CategoryModel, position: Int) {
-            with(binding) {
-                categoriesListCardView.setCardBackgroundColor(categoryModel.color)
-                categoriesListCardView.layoutParams = getViewParams(position)
-                categoryIconImageView.setImageResource(getImageFromResources(categoryModel.categoryImage))
-                categoryTitleTextView.text = categoryModel.categoryName
-            }
-        }
+    inner class ViewHolder(binding: CategoriesListItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        private fun getViewParams(position: Int) : ConstraintLayout.LayoutParams {
-            val params = ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-            if (position % 2 == 0) {
-                params.setMargins(0,0,dipToPx(16),dipToPx(24))
-            } else {
-                params.setMargins(dipToPx(16),0,0,dipToPx(24))
-            }
-            return params
-        }
+        val cardView = binding.categoriesListCardView
+        val imageView = binding.categoryIconImageView
+        val title = binding.categoryTitleTextView
+    }
 
-        private fun dipToPx(dip: Int) : Int {
-            val r: Resources = Resources.getSystem()
-            return TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                dip.toFloat(),
-                r.displayMetrics
-            ).toInt()
+    private fun getViewParams(position: Int) : ConstraintLayout.LayoutParams {
+        val params = ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        if (position % 2 == 0) {
+            params.setMargins(0,0,dipToPx(16),dipToPx(24))
+        } else {
+            params.setMargins(dipToPx(16),0,0,dipToPx(24))
         }
+        return params
+    }
 
-        private fun getImageFromResources(imgId: Int) : Int {
-            return when(imgId) {
-                0 -> diarynote.core.R.drawable.bottom_nav_categories_icon
-                1 -> diarynote.core.R.drawable.work_icon_outline_24
-                2 -> diarynote.core.R.drawable.tech_icon_24
-                3 -> diarynote.core.R.drawable.auto_icon_24
-                4 -> diarynote.core.R.drawable.docs_icon_24
-                5 -> diarynote.core.R.drawable.android_icon_24
-                else -> diarynote.core.R.drawable.bottom_nav_categories_icon
-            }
+    private fun dipToPx(dip: Int) : Int {
+        val r: Resources = Resources.getSystem()
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dip.toFloat(),
+            r.displayMetrics
+        ).toInt()
+    }
+
+    private fun getImageFromResources(imgId: Int) : Int {
+        return when(imgId) {
+            0 -> diarynote.core.R.drawable.bottom_nav_categories_icon
+            1 -> diarynote.core.R.drawable.work_icon_outline_24
+            2 -> diarynote.core.R.drawable.tech_icon_24
+            3 -> diarynote.core.R.drawable.auto_icon_24
+            4 -> diarynote.core.R.drawable.docs_icon_24
+            5 -> diarynote.core.R.drawable.android_icon_24
+            else -> diarynote.core.R.drawable.bottom_nav_categories_icon
         }
     }
 }
