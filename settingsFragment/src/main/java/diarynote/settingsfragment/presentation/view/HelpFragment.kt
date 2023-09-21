d878cadbd6dad6e6ee3dbdacd64301f930a0aa29
profitsw2000 <profitsw2000@gmail.com>
2023-09-21 11:20:48 +0300

    Сделан фрагмент для отображения экрана раздела Помощь.

---
+++ settingsFragment/src/main/java/diarynote/settingsfragment/presentation/view/HelpFragment.kt
@@ -0,0 +1,45 @@
+package diarynote.settingsfragment.presentation.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import diarynote.settingsfragment.R
+import diarynote.settingsfragment.databinding.FragmentHelpBinding
+import diarynote.settingsfragment.presentation.view.adapter.HelpAdapter
+import diarynote.template.utils.OnSettingsMenuItemClickListener
+
+class HelpFragment : Fragment() {
+
+    private var _binding: FragmentHelpBinding? = null
+    private val binding get() = _binding!!
+    private val adapter = HelpAdapter(object : OnSettingsMenuItemClickListener{
+        override fun onItemClick(itemId: Int) {
+            openFragmentById()
+        }
+    })
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentHelpBinding.bind(inflater.inflate(R.layout.fragment_help, container, false))
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        adapter.setData(resources.getStringArray(diarynote.core.R.array.help_menu_strings).toList())
+        binding.helpItemsListRecyclerView.adapter = adapter
+    }
+
+    private fun openFragmentById() {
+
+    }
+}
