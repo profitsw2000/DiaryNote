4a2ce7787564af13e99ca3e31e6ee59a69c385a0
profitsw2000 <profitsw2000@gmail.com>
2023-10-09 16:46:41 +0300

    Создан фрагмент для отображения экрана добавления фото профиля.

---
+++ settingsFragment/src/main/java/diarynote/settingsfragment/presentation/view/account/UserImageFragment.kt
@@ -0,0 +1,24 @@
+package diarynote.settingsfragment.presentation.view.account
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import diarynote.settingsfragment.R
+
+class UserImageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_image, container, false)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
