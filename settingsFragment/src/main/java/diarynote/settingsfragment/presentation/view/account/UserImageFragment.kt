a74c322f36cf7b2f8d85320d4ae533678f5344c2
profitsw2000 <profitsw2000@gmail.com>
2023-10-15 10:17:04 +0300

    Сделан базовый код для фрагмента установки фото профиля.

--- settingsFragment/src/main/java/diarynote/settingsfragment/presentation/view/account/UserImageFragment.kt
+++ settingsFragment/src/main/java/diarynote/settingsfragment/presentation/view/account/UserImageFragment.kt
@@ -1,24 +1,96 @@
 package diarynote.settingsfragment.presentation.view.account
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import diarynote.data.model.UserModel
 import diarynote.settingsfragment.R
+import diarynote.settingsfragment.databinding.FragmentUserImageBinding
+import diarynote.settingsfragment.presentation.viewmodel.SettingsViewModel
+import diarynote.template.model.UserState
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+private const val SELECT_PICTURE = 200
 
 class UserImageFragment : Fragment() {
+
+    private var _binding: FragmentUserImageBinding? = null
+    private val binding get() = _binding!!
+    private val settingsViewModel: SettingsViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_image, container, false)
+        _binding = FragmentUserImageBinding.bind(inflater.inflate(R.layout.fragment_user_image, container, false))
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        observeData()
+    }
+
+    private fun initViews() = with(binding) {
+        profilePhotoImageViewBackground.setOnClickListener {
+
+        }
+    }
+
+    private fun observeData() {
+        val observer = Observer<UserState?> { renderData(it) }
+        settingsViewModel.userLiveData.observe(viewLifecycleOwner, observer)
+    }
+
+    private fun renderData(userState: UserState) {
+        when(userState) {
+            is UserState.Error -> handleError(userState.errorCode, userState.message)
+            UserState.Loading -> setProgressBarVisible(true)
+            is UserState.Success -> handleSuccess(userState.userModel)
+        }
+    }
+
+    private fun handleError(errorCode: Int, message: String) {
+
+    }
+
+    private fun setProgressBarVisible(visible: Boolean) = with(binding) {
+        if (visible) {
+            progressBar.visibility = View.VISIBLE
+            mainGroup.visibility = View.GONE
+        } else {
+            progressBar.visibility = View.GONE
+            mainGroup.visibility = View.VISIBLE
+        }
+    }
+
+    private fun handleSuccess(userModel: UserModel) {
+
+    }
+
+    private fun chooseImage() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.type = "image/*"
+
+        startActivityForResult(Intent.createChooser(intent, "Выберите фото"), SELECT_PICTURE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK && requestCode == SELECT_PICTURE) {
+            val selectedImageUri = data?.data
+
+            selectedImageUri.let {
+                binding.profilePhotoImageView.setImageURI(selectedImageUri)
+            }
+        }
     }
 }
