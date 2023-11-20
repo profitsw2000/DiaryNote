c96d7d48553fa29c3ebcc12320cb0dea4c343e70
profitsw2000 <profitsw2000@gmail.com>
2023-11-20 21:56:47 +0300

    Создан упакованный класс для получения результата запроса на получение списка вопросов раздела Помощь.

---
+++ template/src/main/java/diarynote/template/model/HelpState.kt
@@ -0,0 +1,10 @@
+package diarynote.template.model
+
+import diarynote.data.model.HelpItemModel
+
+sealed class HelpState {
+
+    data class Success(val helpItemModelList: List<HelpItemModel>) : HelpState()
+    data class Error(val message: String) : NotesState()
+    object Loading : NotesState()
+}
