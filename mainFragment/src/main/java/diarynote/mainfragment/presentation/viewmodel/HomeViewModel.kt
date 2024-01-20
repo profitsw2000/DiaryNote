a5ff078e517263556958b2c8bb367d243ba3552f
profitsw2000 <profitsw2000@gmail.com>
2024-01-20 13:15:40 +0300

    в HomeViewModel добавлены две переменные LiveData c постраничным списком и две функции для изменения (получения) этих переменных.

--- mainFragment/src/main/java/diarynote/mainfragment/presentation/viewmodel/HomeViewModel.kt
+++ mainFragment/src/main/java/diarynote/mainfragment/presentation/viewmodel/HomeViewModel.kt
@@ -3,14 +3,18 @@ package diarynote.mainfragment.presentation.viewmodel
 import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.paging.PagedList
 import androidx.sqlite.db.SimpleSQLiteQuery
 import diarynote.core.utils.SearchQueryBuilder
 import diarynote.core.viewmodel.CoreViewModel
 import diarynote.data.domain.CURRENT_USER_ID
 import diarynote.data.interactor.NoteInteractor
 import diarynote.data.mappers.NoteMapper
+import diarynote.data.model.NoteModel
+import diarynote.data.model.type.DataSourceType
 import diarynote.template.model.NotesState
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
@@ -23,8 +27,24 @@ class HomeViewModel(
     private val _notesLiveData = MutableLiveData<NotesState>()
     val notesLiveData: LiveData<NotesState> by this::_notesLiveData
 
+    private lateinit var _notesPagedList: LiveData<PagedList<NoteModel>>
+    val notesPagedList: LiveData<PagedList<NoteModel>> by this::_notesPagedList
+
+    private lateinit var _notesState: LiveData<NotesState>
+    val notesState: LiveData<NotesState> by this::_notesState
+
     fun getNotesList() {
         getAllUserNotes(sharedPreferences.getInt(CURRENT_USER_ID, 0))
+    }
+
+    fun getUserNotesPagedList() {
+        _notesPagedList = noteInteractor.getUserNotesPagedList(
+            viewLifeCycleCompositeDisposable,
+            noteMapper,
+            DataSourceType.UserNotesDataSource,
+            getCurrentUserId(sharedPreferences),
+            false
+        )
     }
 
     private fun getAllUserNotes(userId: Int) {
@@ -65,6 +85,17 @@ class HomeViewModel(
             )
     }
 
+    fun getSearchNotesPagedList(search: String) {
+        _notesPagedList = noteInteractor.getSearchNotesPagedList(
+            viewLifeCycleCompositeDisposable,
+            noteMapper,
+            DataSourceType.UserNotesDataSource,
+            getCurrentUserId(sharedPreferences),
+            search,
+            false
+        )
+    }
+
     private fun getSearchQueryPair(search: String) : SimpleSQLiteQuery {
 
         val searchQueryBuilder = SearchQueryBuilder(
@@ -77,5 +108,9 @@ class HomeViewModel(
         val queryArgs: Array<Any> = searchQueryBuilder.getSearchQueryPair().second.toTypedArray()
 
         return SimpleSQLiteQuery(queryString, queryArgs)
+    }
+
+    private fun getCurrentUserId(sharedPreferences: SharedPreferences): Int {
+        return sharedPreferences.getInt(CURRENT_USER_ID, 0)
     }
 }
