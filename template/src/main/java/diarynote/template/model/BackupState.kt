086035ef7c903187db042f4c43f02766848cc6ee
profitsw2000 <profitsw2000@gmail.com>
2024-02-01 15:32:38 +0300

    Рефакторинг: удаление ненужных файлов.

--- template/src/main/java/diarynote/template/model/BackupState.kt
+++
@@ -1,9 +0,0 @@
-package diarynote.template.model
-
-sealed class BackupState{
-    object SuccessBackup : BackupState()
-    object SuccessRestore : BackupState()
-    data class Error(val message: String, val errorCode: Int) : BackupState()
-    object Loading : BackupState()
-    object Idle : BackupState()
-}
--- template/src/main/java/diarynote/template/model/UserState.kt
+++
@@ -1,9 +0,0 @@
-package diarynote.template.model
-
-import diarynote.data.model.UserModel
-
-sealed class UserState {
-    data class Error(val errorCode: Int, val message: String) : UserState()
-    data class Success(val userModel: UserModel) : UserState()
-    object Loading : UserState()
-}
